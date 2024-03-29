5cff65d074058ccd37c3be1d0f618332f56662d0
Simon Sorg <simon.sorg@student.hpi.de>
2024-03-29 21:34:44 +0100

    add hui formatter

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -351,7 +351,6 @@ def libri_tts(root_path, meta_files=None, ignored_speakers=None):
             meta_files = [os.path.join(root_path, meta_files)]
 
     for meta_file in meta_files:
-        _meta_file = os.path.basename(meta_file).split(".")[0]
         with open(meta_file, "r", encoding="utf-8") as ttf:
             for line in ttf:
                 cols = line.split("\t")
@@ -360,6 +359,41 @@ def libri_tts(root_path, meta_files=None, ignored_speakers=None):
                 _root_path = os.path.join(root_path, f"{speaker_name}/{chapter_id}")
                 wav_file = os.path.join(_root_path, file_name + ".wav")
                 text = cols[2]
+                # ignore speakers
+                if isinstance(ignored_speakers, list):
+                    if speaker_name in ignored_speakers:
+                        continue
+                items.append(
+                    {
+                        "text": text,
+                        "audio_file": wav_file,
+                        "speaker_name": f"LTTS_{speaker_name}",
+                        "root_path": root_path,
+                    }
+                )
+    for item in items:
+        assert os.path.exists(item["audio_file"]), f" [!] wav files don't exist - {item['audio_file']}"
+    return items
+
+def hui(root_path, meta_files=None, ignored_speakers=None):
+    """Normalize the HUI meta data file to TTS format
+    https://opendata.iisys.de/dataset/hui-audio-corpus-german/"""
+    items = []
+    if not meta_files:
+        meta_files = glob(f"{root_path}/**/metadata.csv", recursive=True)
+    else:
+        if isinstance(meta_files, str):
+            meta_files = [os.path.join(root_path, meta_files)]
+
+    for meta_file in meta_files:
+        with open(meta_file, "r", encoding="utf-8") as ttf:
+            for line in ttf:
+                cols = line.split("|")
+                file_name = cols[0]
+                speaker_name = os.path.join(os.path.join(meta_file, os.pardir), os.pardir)
+                book_name = os.path.join(os.path.join(meta_file, os.pardir))
+                wav_file = os.path.join(root_path, speaker_name, book_name, "wavs", file_name + ".wav")
+                text = cols[1]
                 # ignore speakers
                 if isinstance(ignored_speakers, list):
                     if speaker_name in ignored_speakers:
